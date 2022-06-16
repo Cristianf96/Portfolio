11827deace54a2474a273c6603cbd9bacfc0af4a
CristianWhitesoft <cristian.badillo@whitesoft.com.au>
2022-06-15 22:43:16 -0500

    home..

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,58 @@
-import React from 'react'
-import { ChevronDoubleDownIcon } from '@heroicons/react/solid'
+import { useId } from 'react'
+import { CheckCircleIcon, ChevronDoubleDownIcon } from '@heroicons/react/solid'
+import moment from 'moment';
 
 const Home = () => {
+    const id = useId()
+
+    const skills = [
+        'HTML',
+        'CSS',
+        'JAVASCRIPT',
+        'TYPESCRIPT',
+        'NODEJS',
+        'EXPRESS',
+        'REACT',
+        'FIREBASE',
+        'MONGODB',
+        'POSTGRESQL',
+        'MUI',
+        'TAILWIND',
+    ]
+
+    const calcularEdad = () => {
+        var cumpleanos = moment('1996-12-18').format('YYYY-MM-DD');
+        const edad = moment().diff(cumpleanos, 'years', false);
+        return edad;
+    }
+
     return (
-        <div className="mx-auto h-screen flex flex-col items-center justify-center bg-gray-900">
-            <div className='pt-32 md:pt-80 lg:pt-48 flex flex-col justify-center'>
+        <div className="mx-auto h-[cal(100vh-144)] flex flex-col items-center justify-center bg-gray-900">
+            <div className='pt-56 md:pt-96 lg:pt-72 flex flex-col justify-center'>
                 <h1 className='font-mono text-left text-xs pl-16 text-white font-bold md:text-xl md:pl-36 lg:pl-28'>Yo soy</h1>
                 <h1 className='font-mono text-center text-5xl text-white font-bold md:text-8xl'>CRISTIAN</h1>
                 <h1 className='font-mono text-right pr-10 text-xs text-white font-bold md:text-2xl md:pr-28 lg:pr-20'>mid-senior</h1>
                 <h1 className='font-mono text-right pr-0 text-sm text-white font-bold md:text-3xl md:pr-18 lg:pr-0'>FullStack Developer</h1>
             </div>
-            <div className='pt-10 md:pt-40 lg:pt-28 flex flex-col items-center justify-center'>
-                <h1 className='font-mono w-80 md:w-[45rem] lg:w-[80rem] text-xs text-white text-center md:text-lg '>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h1>
+            <div className='pt-36 md:pt-96 lg:pt-80 flex flex-col items-center justify-center'>
+                <h1 className='font-mono pt-5 w-80 md:w-[45rem] lg:w-[80rem] text-xs text-white text-center md:text-xl '>
+                    "El deseo de conocimiento forma a un hombre ― Patrick Rothfuss, The Wise Man's Fear"
+                </h1>
                 <div className='h-16 w-16 flex items-center justify-center'>
                     <ChevronDoubleDownIcon className="h-8 w-8 md:h-12 md:w-12 md:pt-0 lg:h-10 lg:w-10 lg:pt-0 text-white" />
+                </div>
+            </div>
+            <div className='mt-20 mb-20 flex flex-col items-center text-white'>
+                <h1 className='uppercase font-bold pb-5 md:pb-10 md:text-2xl'>competencias</h1>
+                <div className='w-72 md:w-[40rem] lg:w-[60rem] grid grid-rows-4 md:grid-rows-3 lg:grid-rows-2 grid-flow-col gap-2'>
+                    {skills.map((item, i) => {
+                        return (
+                            <div className='flex justify-center' key={i + id}>
+                                <h1 className='font-mono text-xs md:text-xl'>{item}</h1>
+                                <CheckCircleIcon className="h-4 w-4 ml-2 md:h-6 md:w-6 md:pt-0 lg:h-6 lg:w-6 lg:pt-0 text-white" />
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
         </div>
